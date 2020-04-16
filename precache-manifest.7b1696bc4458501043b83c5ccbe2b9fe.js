2e059f1084cadcc01d1e7284b28d355c7b7aff3e
Per Sunde <sunde.per92@gmail.com>
2020-04-16 18:14:11 +0200

    Updates

---
+++ precache-manifest.7b1696bc4458501043b83c5ccbe2b9fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f17fb8004d35580de128350a9703f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b4f40bc9063ee3c3b1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9e260b7d4d53f3803cce",
+    "url": "/static/js/2.b0358a57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0358a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b4f40bc9063ee3c3b1",
+    "url": "/static/js/main.2969ccda.chunk.js"
+  },
+  {
+    "revision": "4fa1ddc4eef8d78d0db8",
+    "url": "/static/js/runtime-main.39f1976d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
