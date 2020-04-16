4029facc97f71cd3cba6931995fadb3793fe9f3c
Per Sunde <sunde.per92@gmail.com>
2020-04-16 20:06:56 +0200

    intro msg

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Per Sunde's personal webpage
         </a>
       </header>
     </div>
