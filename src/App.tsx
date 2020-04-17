c569d040ef68ba40d41825ae93938840d7824c6a
Per Sunde <sunde.per92@gmail.com>
2020-04-17 14:25:04 +0200

    added somed icon links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AnnonPerson from './annonperson.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faGithubSquare,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <figure className="image is-128x128" style={{display: 'inline-block'}}>
+          <img alt="Per Sunde" className="is-rounded" src={AnnonPerson}/>
+        </figure>
+        </div>
+        <div className="Socialmedia-div">
+          <span className="Socialmedia-icon">
+            <a className="link" target="_blank" rel="noopener noreferrer" href="https://github.com/persunde">
+              <FontAwesomeIcon icon={faGithubSquare} color="black" size="2x"/>
+            </a>
+          </span>
+          <span className="Socialmedia-icon">
+            <a className="link" target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/per-sunde-578b68184">
+              <FontAwesomeIcon icon={faLinkedin} color="black" size="2x"/>
+            </a>
+          </span>
+        </div>
+        <div style={{display: 'inline-block'}} className="buttons subtitle">
+          <a href="https://www.sunde.dev/about" className="button is-rounded is-success">About</a>
+          <a className="button is-rounded is-primary" target="_blank" rel="noopener noreferrer" href="https://www.sunde.dev/resume">Resume</a>
+        </div>
+
         <p>
           Per Sunde developer
         </p>
