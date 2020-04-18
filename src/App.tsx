09e676804779557b97f12ef783440e6e6dd13858
Per Sunde <sunde.per92@gmail.com>
2020-04-18 15:54:20 +0200

    created SomedLink component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
 import AnnonPerson from './annonperson.png';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import {
-  faGithubSquare,
-  faLinkedin
-} from '@fortawesome/free-brands-svg-icons';
+import SomedIcons from "./components/somed/SomedIcons"
 
 function App() {
   return (
@@ -17,25 +13,15 @@ function App() {
           <img alt="Per Sunde" className="is-rounded" src={AnnonPerson}/>
         </figure>
         </div>
-        <div className="Socialmedia-div">
-          <span className="Socialmedia-icon">
-            <a className="link" target="_blank" rel="noopener noreferrer" href="https://github.com/persunde">
-              <FontAwesomeIcon icon={faGithubSquare} color="black" size="2x"/>
-            </a>
-          </span>
-          <span className="Socialmedia-icon">
-            <a className="link" target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/per-sunde-578b68184">
-              <FontAwesomeIcon icon={faLinkedin} color="black" size="2x"/>
-            </a>
-          </span>
-        </div>
+        <SomedIcons
+          color="white"
+        />
         <div style={{display: 'inline-block'}} className="buttons subtitle">
           <a href="https://www.sunde.dev/about" className="button is-rounded is-success">About</a>
           <a className="button is-rounded is-primary" target="_blank" rel="noopener noreferrer" href="https://www.sunde.dev/resume">Resume</a>
         </div>
-
         <p>
-          Per Sunde developer
+          Per Sunde fullstack developer
         </p>
       </header>
     </div>
