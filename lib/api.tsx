9ec8ea3b6f623ecd65168538e81f87594e1c1b05
Per Sunde <sunde.per92@gmail.com>
2020-04-30 00:39:37 +0200

    finds img path based on just the img filename

--- lib/api.tsx
+++ lib/api.tsx
@@ -1,6 +1,10 @@
 import fs from 'fs'
+import path from 'path'
+import { readdirSync, statSync } from 'fs'
+
 import { join } from 'path'
 import matter from 'gray-matter'
+import { fail } from 'assert'
 
 const postsDirectory = join(process.cwd(), '_posts')
 
@@ -26,7 +30,11 @@ export function getPostBySlug(slug, fields = []) {
     }
 
     if (data[field]) {
-      items[field] = data[field]
+      if (data[field] == "coverImage" || field == "coverImage") {
+        items[field] = findImgLocation(data[field])
+      } else {
+        items[field] = data[field]
+      }
     }
   })
 
@@ -36,4 +44,34 @@ export function getPostBySlug(slug, fields = []) {
 export function getAllPosts(fields = []) {
   const slugs = getPostSlugs()
   return slugs.map(slug => getPostBySlug(slug, fields))
+}
+
+// coverImage: '/assets/blog/github-pages/github-pages.png'
+// ogImage:
+//   url: '/assets/blog/github-pages/github-pages.png'
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+function findImgLocation(img_name) {
+  const rootFolder = "public/assets/blog/";
+  const folders = getDirectories(rootFolder);
+  
+  for (let folder of folders) {
+    const fromPathTop = path.join( rootFolder, folder );
+    const files = fs.readdirSync( fromPathTop );
+    
+    for( const file of files ) {
+      const fromPathSecond = path.join( fromPathTop, file );
+      const stat = fs.statSync( fromPathSecond );
+      if( stat.isFile() ) {
+          if (file === img_name) {
+            return fromPathSecond.replace("public", "");
+          }
+      }
+    }
+  }
+  fail("Missing image for blog entry", img_name)
 }
