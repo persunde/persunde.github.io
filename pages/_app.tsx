c4afff7544f7c9359b0182db40416e8be457ca4f
Per Sunde <sunde.per92@gmail.com>
2020-05-11 19:02:30 +0200

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { config, library } from '@fortawesome/fontawesome-svg-core'
+import Router from 'next/router'
+import { initGA, logPageView } from '../lib/analytics'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 import { faBars, faUser } from '@fortawesome/free-solid-svg-icons'
@@ -12,5 +14,11 @@ import '../node_modules/highlight.js/styles/github.css';
 
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    initGA()
+    logPageView()
+    Router.events.on('routeChangeComplete', logPageView)
+  }, [])
+  
   return <Component {...pageProps} />
 }
