6bf70f389fe8ad2e0b58e3669e5b4c23480c935e
Per Sunde <sunde.per92@gmail.com>
2021-01-03 22:32:31 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4a4d2b1c29abecc33b73";
+/******/ 	var hotCurrentHash = "54e19490a5ecedf9ba6e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
