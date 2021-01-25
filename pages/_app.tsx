8cd330de3f69f164ba80c7f81048a927f8b366db
Per Sunde <sunde.per92@gmail.com>
2021-01-25 21:22:43 +0100

    made analytics only live for production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,9 +12,13 @@ import '../node_modules/highlight.js/styles/github.css';
 
 
 export default function MyApp({ Component, pageProps }) {
+  let analytics = null
+  if (process.env.NODE_ENV === "production") {
+    analytics = <script async defer data-website-id="cf736c86-83fe-4aa8-a589-dcdc16b8498e" src="https://analytics.nordictechjobs.com/umami.js"></script>
+  }
   return (
     <>
-      <script async defer data-website-id="cf736c86-83fe-4aa8-a589-dcdc16b8498e" src="https://analytics.nordictechjobs.com/umami.js"></script>
+      {analytics}
       <Component {...pageProps} />
     </>
   )
