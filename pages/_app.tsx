70892d765b6bb76f37893ae3001a712450ec286a
Per Sunde <sunde.per92@gmail.com>
2021-01-31 00:19:45 +0100

    changed analytics link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ import '../node_modules/highlight.js/styles/github.css';
 export default function MyApp({ Component, pageProps }) {
   let analytics = null
   if (process.env.NODE_ENV === "production") {
-    analytics = <script async defer data-website-id="cf736c86-83fe-4aa8-a589-dcdc16b8498e" src="https://analytics.nordictechjobs.com/umami.js"></script>
+    analytics = <script async defer data-website-id="74444df2-7306-41d7-b127-6463fcb046c3" src="https://analytics.nordictechjobs.com/umami.js"></script>
   }
   return (
     <>
